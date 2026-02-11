749cdc645d6eb1a784d104a9ee8d52b62d7269b2
Derek Berbo <116178800+pickachuuu@users.noreply.github.com>
2026-02-11 16:39:47 +0800

    feat: add r2 for cdn stored images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-5d0fe94a3da5458ca88e4e79220a6798.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
