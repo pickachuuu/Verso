ec867c089c0a417544b4d2a54616bcc8a1b48f8a
Derek Berbo <116178800+pickachuuu@users.noreply.github.com>
2026-04-07 11:01:23 +0800

    feat: add caching strategies

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
 });
 
 const securityHeaders = [
